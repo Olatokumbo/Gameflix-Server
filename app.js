2d9d4b6b75ef958060964ef391fa857b9ffc9294
David Faith <faithodesola@gmail.com>
2021-11-03 23:43:09 +0000

    Added a welcome message

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use("/review", review);
 app.use("/admin", admin);
 
 
-app.get("/ping", (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json({
     response: "Welcome to Gameflix Server",
   });
